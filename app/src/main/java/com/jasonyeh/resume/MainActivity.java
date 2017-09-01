0931310f3c4e4b2e9d0a233d5acb60aa2dc25b6d
Jason Yeh <wadejason80@gmail.com>
2017-09-01 11:27:23 -0700

    Add fake data to display - multi educations

--- app/src/main/java/com/jasonyeh/resume/MainActivity.java
+++ app/src/main/java/com/jasonyeh/resume/MainActivity.java
@@ -2,6 +2,8 @@ package com.jasonyeh.resume;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.jasonyeh.resume.model.BasicInfo;
@@ -14,7 +16,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private BasicInfo basicInfo;
-    private Education education;
+//    private Education education;
+    private List<Education> educations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupBasicInfoUI();
-        setupEducationUI();
+//        setupEducationUI();
+//        setupEducationsUI();
+
+        setupEducations();
+
     }
 
     private void setupBasicInfoUI() {
@@ -36,34 +43,98 @@ public class MainActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.name)).setText(basicInfo.name);
         ((TextView) findViewById(R.id.email)).setText(basicInfo.email);
     }
+    // single education
+//    private void setupEducationUI() {
+//        // Display the education data onto the UI
+//        // Follow the example in setupBasicInfoUI
+//        // You will probably find formatItems method useful when displaying the courses
+//
+//        String dateRangeStr = DateUtils.dateToString(education.startDate)
+//                + " ~ " + DateUtils.dateToString(education.endDate);
+//
+//        ((TextView) findViewById(R.id.education_school)).setText(education.school + " (" + dateRangeStr + ")");
+//        ((TextView) findViewById(R.id.education_courses)).setText(formatItems(education.courses));
+//    }
 
-    private void setupEducationUI() {
-        // Display the education data onto the UI
-        // Follow the example in setupBasicInfoUI
-        // You will probably find formatItems method useful when displaying the courses
+    // multi educations
+//    private void setupEducationsUI() {
+//        LinearLayout educationsLayout = (LinearLayout) findViewById(R.id.education_list);
+//        for (Education education : educations) {
+//            //把佈局文件轉成樹
+//            View view = getLayoutInflater().inflate(R.layout.education_item, null);
+//            // convert education object into view
+//            String dateRangeStr = DateUtils.dateToString(education.startDate)
+//                + " ~ " + DateUtils.dateToString(education.endDate);
+//
+//            ((TextView) view.findViewById(R.id.education_school)).setText(
+//                    education.school + " (" + dateRangeStr + ")");
+//            ((TextView) view.findViewById(R.id.education_courses)).setText(
+//                    formatItems(education.courses));
+//            // view ->介面上
+//            educationsLayout.addView(view);
+//
+//        }
+//    }
 
+    // multi education 代碼重構 Code refactoring
+    private void setupEducations() {
+        LinearLayout educationsLayout = (LinearLayout) findViewById(R.id.education_list);
+        for (Education education : educations) {
+            educationsLayout.addView(getEducationView(education));
+        }
+    }
+    // 轉換器
+    private View getEducationView(Education education) {
+        View view = getLayoutInflater().inflate(R.layout.education_item, null);
+        // convert education object into view
         String dateRangeStr = DateUtils.dateToString(education.startDate)
                 + " ~ " + DateUtils.dateToString(education.endDate);
 
-        ((TextView) findViewById(R.id.education_school)).setText(education.school + " (" + dateRangeStr + ")");
-        ((TextView) findViewById(R.id.education_courses)).setText(formatItems(education.courses));
+        ((TextView) view.findViewById(R.id.education_school)).setText(
+                education.school + " (" + dateRangeStr + ")");
+        ((TextView) view.findViewById(R.id.education_courses)).setText(
+                formatItems(education.courses));
+        // view ->介面上
+        return view;
     }
+
 
     private void fakeData() {
         basicInfo = new BasicInfo();
         basicInfo.name = "Jason";
         basicInfo.email = "jasonyeh@xxx.com";
 
-        education = new Education();
-        education.school = "SJSU";
-        education.major = "Computer Engineering";
-        education.startDate = DateUtils.stringToDate("08/2015");
-        education.endDate = DateUtils.stringToDate("08/2017");
+        educations = new ArrayList<>();
 
-        education.courses = new ArrayList<>();
-        education.courses.add("Database");
-        education.courses.add("Algorithm");
-        education.courses.add("Network Security");
+        // education1
+        Education education1 = new Education();
+        education1 = new Education();
+        education1.school = "SJSU";
+        education1.major = "Computer Engineering";
+        education1.startDate = DateUtils.stringToDate("08/2015");
+        education1.endDate = DateUtils.stringToDate("08/2017");
+
+        education1.courses = new ArrayList<>();
+        education1.courses.add("Database");
+        education1.courses.add("Algorithm");
+        education1.courses.add("Network Security");
+
+        educations.add(education1);
+
+        // education2
+        Education education2 = new Education();
+        education2 = new Education();
+        education2.school = "NTNU";
+        education2.major = "Mathematics";
+        education2.startDate = DateUtils.stringToDate("09/2009");
+        education2.endDate = DateUtils.stringToDate("06/2013");
+
+        education2.courses = new ArrayList<>();
+        education2.courses.add("Linear Algebra");
+        education2.courses.add("Calculus");
+        education2.courses.add("Discrete Mathematics");
+
+        educations.add(education2);
 
     }
 
